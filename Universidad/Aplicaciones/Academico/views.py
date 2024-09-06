6eaae66c2582bf5c67948f76e34032c08ee67b2b
Juan Pablo <juanpablocaceres8@gmail.com>
2024-09-05 23:03:06 -0300

    Proyecto terminado

--- Universidad/Aplicaciones/Academico/views.py
+++ Universidad/Aplicaciones/Academico/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Curso
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     cursos = Curso.objects.all()
+    messages.success(request, '¡Cursos listados!')
     return render(request, 'gestionCursos.html', {"cursos": cursos})
 
 def registrarCurso(request):
@@ -12,11 +14,15 @@ def registrarCurso(request):
     creditos = request.POST['numCreditos']
 
     curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    messages.success(request, '¡Curso registrado!')
+
     return redirect("/")
 
 def eliminarCurso(request, codigo):
     curso = Curso.objects.get(codigo=codigo)
     curso.delete()
+    messages.success(request, '¡Curso eliminado!')
+
     return redirect("/")
 
 def edicionCurso(request, codigo):
@@ -33,5 +39,8 @@ def editarCurso(request):
     curso.nombre = nombre
     curso.creditos = creditos
     curso.save()
+
+    messages.success(request, '¡Curso actualizado!')
+
 
     return redirect("/")    
